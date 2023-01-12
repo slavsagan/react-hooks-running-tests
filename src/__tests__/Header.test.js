09fba69a8ec2881bc03c3024207be6348b664c56
Slav <itiksolution@gmail.com>
2023-01-12 11:22:58 +1100

    ass complete

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -7,5 +7,8 @@ import Header from "../components/Header";
 test("displays the text 'hello from the Header!'", () => {
   render(<Header />);
 
+  const element = screen.queryAllByText('hello from the Header!')
+  screen.debug(element)
+
   expect(screen.queryByText("hello from the Header!")).toBeInTheDocument();
 });
